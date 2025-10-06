25911d1c54c9b6a0101da5b6aae505d12ad9a78f
Ankit Sharma <ankit.sharma@filuet.com>
2025-10-06 14:10:55 +0530

    test 5

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,17 @@
 import { JSX } from 'react';
 
 function App(): JSX.Element {
-  return <h1>I am updating the text and testing for level 1 </h1>;
+  return (
+    <h1>
+      {' '}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aut rem quidem aspernatur
+      perferendis nisi et sequi odit, veritatis at explicabo praesentium accusamus pariatur,
+      voluptas accusantium? Facilis nesciunt impedit natus? Cum ullam possimus debitis quidem
+      deserunt eveniet fugiat illum. Dignissimos porro error unde eligendi alias odio, maiores
+      sequi, maxime adipisci voluptatum a. Odio voluptate ipsam officiis tempora praesentium,
+      perspiciatis adipisci!{' '}
+    </h1>
+  );
 }
 
 export default App;
