1183e766e72ae624abc897ba6de2549b2cc80b9b
Ankit Sharma <ankit.sharma@filuet.com>
2025-10-06 13:04:48 +0530

    test: updating the text

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 import { JSX } from 'react';
 
 function App(): JSX.Element {
-  return <h1>hiii yrr chal to gya </h1>;
+  return <h1>I am updating the text and testing for level 1 </h1>;
 }
 
 export default App;
